815428fb846725dc34330cb6a562376ee26bbc1d
szmisztal <sz.misztal@gmail.com>
2025-05-25 11:39:05 +0200

    work with controllers

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-# Example file showing a basic pygame "game loop"
 import pygame
+from config import screen_resolution, running, green_color, blue_color, red_color, white_color
+from objects.snake_obj import SnakeObject
 
-# pygame setup
+
 pygame.init()
-screen = pygame.display.set_mode((720, 480))
+screen = pygame.display.set_mode(screen_resolution)
 clock = pygame.time.Clock()
-running = True
+snake = SnakeObject(screen_resolution[0]/2, screen_resolution[1]/2, 10, 10)
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("green")
-    pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(10, 5, 11, 5))
-    # RENDER YOUR GAME HERE
+    pygame.draw.rect(screen, blue_color, pygame.Rect(snake.x_pos, snake.y_pos, snake.width, snake.height))
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)
 
 pygame.quit()
