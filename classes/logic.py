e9b8d607ef4f987a7cc4686db7ab20a08a622fbd
szmisztal <sz.misztal@gmail.com>
2025-05-27 13:01:01 +0200

    add change apple coords when it was eated by snake

--- classes/logic.py
+++ classes/logic.py
@@ -1,5 +1,6 @@
 import pygame
 from classes.snake_obj import SnakeObject
+from classes.apple_obj import AppleObject
 from config import screen_resolution
 
 
@@ -7,13 +8,18 @@ class GameLogic:
     def __init__(self, screen_resolution):
         self.running = True
         self.snake = SnakeObject(screen_resolution)
+        self.apple = AppleObject(screen_resolution)
         self.clock = pygame.time.Clock()
 
     def collision_with_screen_edge(self, screen_resolution):
-        if self.snake.snake_parts_position[0][0] < 0 or self.snake.snake_parts_position[0][0] >= screen_resolution[0]:
+        if self.snake.parts_position[0][0] < 0 or self.snake.parts_position[0][0] >= screen_resolution[0]:
             self.running = False
-        if self.snake.snake_parts_position[0][1] < 0 or self.snake.snake_parts_position[0][1] >= screen_resolution[1]:
+        if self.snake.parts_position[0][1] < 0 or self.snake.parts_position[0][1] >= screen_resolution[1]:
             self.running = False
+
+    def eating_an_apple(self):
+        if self.snake.parts_position[0][0] + 5 == self.apple.coordinates[0] and self.snake.parts_position[0][1] + 5 == self.apple.coordinates[1]:
+            self.apple.new_coordinates(screen_resolution)
 
     def game_loop(self, screen):
         while self.running:
@@ -29,8 +35,9 @@ class GameLogic:
                         self.snake.movement_direction = "LEFT"
                     elif event.key == pygame.K_RIGHT and self.snake.movement_direction != "LEFT":
                         self.snake.movement_direction = "RIGHT"
-            screen.screen_draw(self.snake)
+            screen.screen_draw(self.snake, self.apple)
             self.snake.snake_movement()
+            self.eating_an_apple()
             self.collision_with_screen_edge(screen_resolution)
             pygame.display.flip()
             self.clock.tick(10)
