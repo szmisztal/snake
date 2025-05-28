c33f30626971752378e805292d3f0ea74ef75193
szmisztal <sz.misztal@gmail.com>
2025-05-28 10:12:51 +0200

    fix establish_coordinates func for apple

--- classes/apple_obj.py
+++ classes/apple_obj.py
@@ -6,8 +6,8 @@ class AppleObject:
         self.coordinates = self.establish_coordinates(screen_resolution)
 
     def establish_coordinates(self, screen_resolution):
-        x = round(random.randint(5, screen_resolution[0] + 5), -1)
-        y = round(random.randint(5, screen_resolution[1] + 5), -1)
+        x = round(random.randint(5, screen_resolution[0]), -1)
+        y = round(random.randint(5, screen_resolution[1]), -1)
         return (x - 5, y - 5)
 
     def new_coordinates(self, screen_resolution):
