4341c2f787f129953753e1dd12408a757199c62a
szmisztal <sz.misztal@gmail.com>
2025-05-30 17:01:28 +0200

    fix bug - apple appears only in screen res now

--- classes/apple_obj.py
+++ classes/apple_obj.py
@@ -8,7 +8,7 @@ class AppleObject:
     def establish_coordinates(self, screen_resolution):
         x = round(random.randint(5, screen_resolution[0]), -1)
         y = round(random.randint(5, screen_resolution[1]), -1)
-        return (x + 5, y + 5)
+        return (x - 5, y - 5)
 
     def new_coordinates(self, screen_resolution):
         self.coordinates = self.establish_coordinates(screen_resolution)
