444e712109ec7a335083a939d53db5a8e43c4d6c
szmisztal <sz.misztal@gmail.com>
2025-05-30 09:26:59 +0200

    fix eating an apple func

--- classes/logic.py
+++ classes/logic.py
@@ -5,11 +5,12 @@ class GameLogic:
             return False
 
     def collision_snake_with_his_tail(self, snake):
-        if snake.parts_position[0] in snake.parts_position[1:]:
+        if snake.parts_position[0] in snake.parts_position[2:]:
             return False
 
     def eating_an_apple(self, screen_resolution, snake, apple):
         if snake.parts_position[0][0] + 5 == apple.coordinates[0] and snake.parts_position[0][1] + 5 == apple.coordinates[1]:
+            snake.parts_position.insert(1, [snake.parts_position[0][0], snake.parts_position[0][1]])
             apple.new_coordinates(screen_resolution)
 
 
